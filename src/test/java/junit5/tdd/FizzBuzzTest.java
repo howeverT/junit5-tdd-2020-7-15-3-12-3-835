e942b0b6f0f0247e3100c7f27eec557cce7d2763
howevert <1413537501@qq.com>
2020-07-23 11:48:42 +0800

    [fizzbuzz][david] bug-fix: rename should_return_1_when_count_off_given_1

--- src/test/java/junit5/tdd/FizzBuzzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
     @Test
-    void should_return_fizz_when_count_off_given_1() {
+    void should_return_1_when_count_off_given_1() {
         //given
         FizzBuzz fizzBuzz=new FizzBuzz();
 
@@ -16,4 +16,5 @@ public class FizzBuzzTest {
         //then
         assertEquals("1",result);
     }
+    
 }
