0c90c3040d83d5d037023a970abea703648e7d6d
howevert <1413537501@qq.com>
2020-07-23 13:43:16 +0800

    [fizzbuzz][david] feature: pass should_return_fizzbuzz_when_count_off_given_15

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -3,6 +3,8 @@ package junit5.tdd;
 public class FizzBuzz {
 
     public String play(int inputNumber){
+        if (inputNumber%15==0)
+            return "fizzbuzz";
         if (inputNumber%3==0)
             return "fizz";
         if (inputNumber%5==0)
